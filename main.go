e4ca7e149d0b815e15eed00d03ccee779cb789ee
iamnoah <noahispas@gmail.com>
2022-08-11 19:42:24 +0200

    tmo

--- main.go
+++ main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Some input vat:", os.Getenv("INPUT_API_KEY"))
+	fmt.Println("Some input vat:", os.Getenv("INPUT_API_TOKEN"))
 	fmt.Println("::set-output name=output_var::someOutputValue")
 }
